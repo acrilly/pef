6a5f0fd75a2303adb638c7d033ac9aea05e7d25b
Andrew Crilly <andrew.crilly@triad.co.uk>
2017-01-27 11:25:42 +0000

    Restored to toolkit 3

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Political Party Registration Service",
+  serviceName: "Service name goes here",
 
   // Default port that prototype runs on
   port: '3000',
